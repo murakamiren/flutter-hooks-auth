8cde666651a4d6aba6008c05fe2399d336c2b6da
renmurakami <autoeyy@gmail.com>
2022-06-22 11:44:11 +0900

    uid email

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:riverpod_hooks/utils/auth.dart';
 import 'package:riverpod_hooks/views/login.dart';
 
@@ -19,8 +21,10 @@ class HomeView extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text("welcome"),
-              SizedBox(
+              const Text("welcome"),
+              Text("uid: ${auth.currentUser!.uid}"),
+              Text("email: ${auth.currentUser!.email!}"),
+              const SizedBox(
                 height: 20,
               ),
               ElevatedButton(
